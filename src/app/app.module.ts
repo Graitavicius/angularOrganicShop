5353cec00d50fa2867ab3c60702feba8bc56a5c2
Graitavicius <grait.mindaugas@gmail.com>
2020-10-31 20:20:51 +0200

    first commit, added bootstrap and angularfire

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireAuthModule } from '@angular/fire/auth'
+import { AngularFireDatabaseModule } from '@angular/fire/database'
 
 import { AppComponent } from './app.component';
+import { environment } from 'src/environments/environment';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule,
+    AngularFireAuthModule,
+    AngularFireDatabaseModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyB_sOhf3iGMDSV2lWULcGBmPJ5x1_6HIdY",
+    authDomain: "orgshop-9e698.firebaseapp.com",
+    databaseURL: "https://orgshop-9e698.firebaseio.com",
+    projectId: "orgshop-9e698",
+    storageBucket: "orgshop-9e698.appspot.com",
+    messagingSenderId: "769776252855",
+    appId: "1:769776252855:web:da2abbc9240fa77efb5a32",
+    measurementId: "G-B959ZKM997"
+  }
 };
