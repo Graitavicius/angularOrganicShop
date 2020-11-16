cb0b7d3e857b8a9accc83b34dbf24443398aa511
Graitavicius <grait.mindaugas@gmail.com>
2020-11-16 19:43:28 +0200

    editing and updating products

--- src/app/admin/product-form/product-form.component.ts
+++ src/app/admin/product-form/product-form.component.ts
@@ -1,7 +1,9 @@
+import { Product } from './../../models/product.model';
 import { ProductService } from './../../services/product.service';
 import { CategoryService } from './../../services/category.service';
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { Router, ActivatedRoute } from '@angular/router';
+import { take } from 'rxjs/operators';
 
 @Component({
   selector: 'app-product-form',
@@ -10,15 +12,29 @@ import { Router } from '@angular/router';
 })
 export class ProductFormComponent implements OnInit {
   categories$;
+  product;
+  id;
 
   constructor(private categoryService: CategoryService,
               private productService: ProductService,
-              private router: Router) {
+              private router: Router,
+              private route: ActivatedRoute) {
     this.categories$ = categoryService.getCategories();
+
+    this.product = {};
+    this.id = this.route.snapshot.paramMap.get('id');
+    if (this.id) {
+      this.productService.get(this.id)
+      .pipe(take(1)).subscribe(product => this.product = product);
+    }
   }
 
   save(product) {
-    this.productService.create(product);
+    if (this.id) {
+      this.productService.update(this.id, product);
+    } else {
+      this.productService.create(product);
+    }
     this.router.navigate(['/admin/products']);
   }
 
--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,3 +1,4 @@
+import { Product } from './../models/product.model';
 import { AngularFireDatabase } from '@angular/fire/database';
 import { Injectable } from '@angular/core';
 import { map } from 'rxjs/operators';
@@ -21,5 +22,13 @@ export class ProductService {
       .pipe(map(changes => changes.map(c => ({
         $key: c.payload.key, $value: c.payload.val()
       }))));
+  }
+
+  get(productId) {
+    return this.db.object('/products/' + productId).valueChanges();
+  }
+
+  update(productId, product) {
+    return this.db.object('/products/' + productId).update(product);
   }
 }
